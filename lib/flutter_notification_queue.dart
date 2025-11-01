7686e93803eafacd1901fa0e31029a04e40026cf
PooriaAskariM <pooriaaskarim@gmail.com>
2025-11-02 01:21:48 +0330

    Chore: Refactor library exports for better organization
    This commit reorganizes the library's public API by adjusting the exported files.
    
    The `notification_manager.dart` export has been moved to a more specific path under `src/notification_wrapper/notification_manager/notification_manager.dart`, and its internal `NotificationManager` class is now hidden.
    
    Additionally, new files are now exported:
    - `src/notification_wrapper/notification_wrapper.dart`
    - `src/notification_wrapper/overlay_manager/overlay_manager.dart`
    
    Internal utility and model classes from `overlay_manager.dart` and `utils.dart` have been explicitly hidden from the public API to prevent unintended use.

--- lib/flutter_notification_queue.dart
+++ lib/flutter_notification_queue.dart
@@ -3,6 +3,16 @@ library;
 export 'src/enums/enums.dart' hide OnDrag, OnLongPress;
 export 'src/notification/notification.dart' hide NotificationActionType;
 export 'src/notification_channel/notification_channel.dart';
-export 'src/notification_manager/notification_manager.dart';
 export 'src/notification_queue/notification_queue.dart';
-export 'src/utils/utils.dart' hide Utils;
+export 'src/notification_wrapper/notification_manager/notification_manager.dart'
+    hide NotificationManager;
+export 'src/notification_wrapper/notification_wrapper.dart';
+export 'src/notification_wrapper/overlay_manager/overlay_manager.dart'
+    hide
+        AbsolutePosition,
+        AlignedPosition,
+        AnchoredPosition,
+        OverlayEntryData,
+        OverlayManager,
+        OverlayPosition;
+export 'src/utils/utils.dart' hide ColorUtils, Utils;
